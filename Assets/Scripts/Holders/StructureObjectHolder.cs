3cb80436b002bb699a847cf0306c92cc2c480a85
LIM PEI SHENG <164347E@sidm.student.local>
2018-02-12 18:11:40 +0800

    Added StructureOBJ prefab holder script Added Floor generator Scipt

---
+++ Assets/Scripts/Holders/StructureObjectHolder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructureObjectHolder : MonoBehaviour {
+
+    public GameObject Floor;
+    public GameObject StoneWall;
+    public GameObject StonePillar;
+
+}
