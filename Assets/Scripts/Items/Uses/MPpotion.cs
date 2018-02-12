b9e8fa0439faa8f23c3b443e4ab77c178d01d82a
NICHOLAS POH JUN HENG <164353M@sidm.student.local>
2018-02-12 17:44:58 +0800

    Added Itembase, ItemManager and sample Items

---
+++ Assets/Scripts/Items/Uses/MPpotion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MPpotion : MonoBehaviour, ItemBase
+{
+    Image ItemImage;
+
+    public int getCost()
+    {
+        return 500;
+    }
+
+    public Image getItemImage()
+    {
+        return ItemImage;
+    }
+
+    public string getName()
+    {
+        return "MPpotion";
+    }
+
+    public string getType()
+    {
+        return "Uses";
+    }
+
+    public bool Owned()
+    {
+        Debug.Log("MPpotion Owned not done");
+        return true;
+    }
+}
---
+++ Assets/Scripts/Items/Weapon/Sword.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sword : MonoBehaviour , ItemBase
+{
+    Image ItemImage;
+
+    public int getCost()
+    {
+        return 1000;
+    }
+
+    public Image getItemImage()
+    {
+        return ItemImage;
+    }
+
+    public string getName()
+    {
+        return "Sword";
+    }
+
+    public string getType()
+    {
+        return "Weapons";
+    }
+
+    public bool Owned()
+    {
+        Debug.Log("Sword Owned not done");
+        return true;
+    }
+}
