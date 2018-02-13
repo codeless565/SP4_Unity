102be6c362af599ffdc3e7231938e8b1d5a55cce
Jolyn Wong <shiroyami08@gmail.com>
2018-02-13 11:12:56 +0800

    StatsBase

---
+++ Assets/Scripts/Player/StatsBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Stats of Objects */
+public interface StatsBase
+{
+    string GetName();
+    int GetHealth();
+    float GetAttack();
+    //float GetDefense();
+    float GetMoveSpeed();
+
+
+
+
+}
