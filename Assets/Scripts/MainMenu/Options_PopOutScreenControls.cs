0d5d3a584bee88b0e0d3f0cf54bbe13db380da73
GOH LIANGLI <163464J@sidm.student.local>
2018-02-13 15:39:51 +0800

    Added in a Skeleton of a Workable Music Volume Slider.

--- Assets/Scripts/MainMenu/Options_PopOutScreenControls.cs
+++ Assets/Scripts/MainMenu/Options_PopOutScreenControls.cs
@@ -6,21 +6,46 @@ using UnityEngine.UI;
 public class Options_PopOutScreenControls : MonoBehaviour {
 
     [SerializeField]
-    Button m_backButton;
+    Button m_backButton, m_applyButton;
 
     [SerializeField]
     GameObject Options_PopOutScreen;
 
+    [SerializeField]
+    GameObject backgroundMusic;
+
+    // Background Music
+    public Slider musicVolumeSlider;
+    private AudioSource musicSource;
 
     // Use this for initialization
-    void Start () {	}
-	
-	// Update is called once per frame
-	void Update () {}
+    void Start()
+    {
+        musicSource = backgroundMusic.GetComponent<AudioSource>();
+        // Setting the musicVolumeSlider value.
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("MainMenu_BGM_Volume", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Changing the BGM volume based on the Slider value.
+        musicSource.volume = musicVolumeSlider.value;
+    }
+
+    // When Apply Button is Pressed.
+    public void ApplyButtonPressed()
+    {
+        // Save the Changes made to the MusicVolume Slider.
+        PlayerPrefs.SetFloat("MainMenu_BGM_Volume", musicVolumeSlider.value);
+    }
 
     // When Back Button is Pressed.
     public void BackButtonPressed()
     {
+        // If Back is Pressed w/o saving, the music volume will stay the same.
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("MainMenu_BGM_Volume", 1);
+
         // Set Active to False, Options Pop-Out Screen will not be rendered.
         Options_PopOutScreen.SetActive(false);
     }
