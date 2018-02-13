4a62e7ab14e749ddc313d1ac829102562354fd4f
LIANG LERAN JENNY <141548S@sidm.student.local>
2018-02-13 15:00:40 +0800

    Textbox added

---
+++ Assets/Scripts/Game/Dialog Box/TextBoxManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour{
+
+    public GameObject textBox;
+    public Text theText;
+
+    public TextAsset textfile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+    public PlayerManager player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<PlayerManager>();
+
+        if (textfile != null)
+        {
+            //getting line of text
+            textLines = (textfile.text.Split('\n'));
+        }
+
+        if(endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+
+        }
+    }
+
+    void Update()
+    {
+        theText.text = textLines[currentLine];
+
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            currentLine += 1;
+        }
+    }
+}
