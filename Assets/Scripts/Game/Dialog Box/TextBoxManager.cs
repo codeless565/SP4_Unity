fa26b83db2b9119959ed58a85a95a09b169fc0b3
LIANG LERAN JENNY <141548S@sidm.student.local>
2018-02-14 11:48:31 +0800

    Basic Tutorial done, waiting for item and enemy interactions done before finalised tutorial stage. Textbox manager is complete. TextboxActivate which is used for item/npc interaction is done.

--- Assets/Scripts/Game/Dialog Box/TextBoxManager.cs
+++ Assets/Scripts/Game/Dialog Box/TextBoxManager.cs
@@ -23,26 +23,13 @@ public class TextBoxManager : MonoBehaviour
     public bool stopPlayerMovement;
 
     //Typewriter
-    private bool isTyping = false;
-    private bool cancelTyping;
+    public bool isTyping = false;
+    public bool cancelTyping;
     public float typeSpeed;
-
-    //For tutorial
-    bool tut;
-    private bool movedW, movedA, movedS, movedD;
 
     // Use this for initialization
     void Start()
     {
-        Scene currentScene = SceneManager.GetActiveScene();
-        if (currentScene.name == "SceneTutorial")
-        {
-            tut = true;
-            movedW = false;
-            movedA = false;
-            movedS = false;
-            movedD = false;
-        }
 
         player = FindObjectOfType<PlayerManager>();
         isActive = true;
@@ -79,6 +66,7 @@ public class TextBoxManager : MonoBehaviour
 
         //show text
         //theText.text = textLines[currentLine];
+
         if (Input.GetKeyDown(KeyCode.Return))
         {
             if (!isTyping)
@@ -91,13 +79,8 @@ public class TextBoxManager : MonoBehaviour
                 }
                 else
                 {
-                    if (tut)
-                    {
-                        TutorialUpdate();
-                    }
-                    else
-                        //show text letter by letter
-                        StartCoroutine(TypeText(textLines[currentLine]));
+                    //show text letter by letter
+                    StartCoroutine(TypeText(textLines[currentLine]));
                 }
             }
             else if (isTyping && !cancelTyping) //interrupts typing
@@ -105,9 +88,10 @@ public class TextBoxManager : MonoBehaviour
                 cancelTyping = true;
             }
         }
+
     }
 
-    private IEnumerator TypeText(string lineOfText)
+    public IEnumerator TypeText(string lineOfText)
     {
         int letter = 0;
         theText.text = "";
@@ -129,7 +113,6 @@ public class TextBoxManager : MonoBehaviour
     public void EnableTextBox()
     {
         textBox.SetActive(true);
-
         //Player can't move when Textbox is active
         if (stopPlayerMovement)
         {
@@ -153,66 +136,5 @@ public class TextBoxManager : MonoBehaviour
             textLines = new string[1];
             textLines = (theText.text.Split('\n'));
         }
-    }
-
-    void TutorialUpdate()
-    {
-        switch (currentLine)
-        {
-            case 2: //after movement message
-                if (!movedA && !movedD && !movedS && !movedW)
-                {
-                    DisableTextBox();
-                    if (Input.GetKeyDown(KeyCode.W))
-                    {
-                        movedW = true;
-                    }
-                    if (Input.GetKeyDown(KeyCode.A))
-                    {
-                        movedA = true;
-                    }
-                    if (Input.GetKeyDown(KeyCode.S))
-                    {
-                        movedS = true;
-                    }
-                    if (Input.GetKeyDown(KeyCode.D))
-                    {
-                        movedD = true;
-                    }
-                }
-                else
-                {
-                    currentLine = 2; //give instructions about attack
-                }
-                EnableTextBox();
-                break;
-
-            case 3: //attack enemy
-                DisableTextBox();
-                if (Input.GetMouseButton(0)) //tried weapon //will change trigger to when enemy killed when enemy spawning is available
-                {
-                    currentLine = 3; //give instructions about changing weapon
-                    EnableTextBox();
-                }
-                break;
-
-            case 4: //change weapon
-                if (Input.GetKeyDown(KeyCode.C))
-                {
-                    currentLine = 4; //give instructions about interactions
-                    EnableTextBox();
-                }
-                break;
-
-            case 5: //interact
-                if (Input.GetKeyDown(KeyCode.I)) //will change to when item has been succesfully interacted
-                {
-                    currentLine = 5; //end message
-                    EnableTextBox();
-                }
-                break;
-        }
-        //show text letter by letter
-        StartCoroutine(TypeText(textLines[currentLine]));
     }
 }
