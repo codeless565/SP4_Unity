3f16f86d1411cb1e486bdd92fd9f3efd8a29c48b
Jolyn Wong <shiroyami08@gmail.com>
2018-02-15 07:31:42 +0800

    Updated Player Attack ( still improving if continuing with 3D ) Added a test 2D scene for 2D development

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Attack System for the Player */
+public class PlayerAttack : MonoBehaviour
+{
+    /* For Attacking */
+    [SerializeField]
+    private GameObject melee; // to detect for melee
+    private float m_timer;
+
+    GameObject temp;
+
+    private Vector3 pos;
+
+    void Start()
+    {
+        m_timer = 1.0F;
+    }
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+		// To fire a small bullet forward when player swings
+        if (Input.GetMouseButtonDown(0) && !temp) // dont allow spam
+        {
+            //create a bullet
+            temp = Instantiate(melee, transform.position, transform.rotation);
+            m_timer = 1.0F;
+        }
+
+        if (m_timer <= 0.0F)
+            DestroyImmediate(temp);
+
+        if (temp)
+        {
+            m_timer -= Time.deltaTime;
+            temp.transform.position += transform.parent.transform.forward * Time.deltaTime * 0.5f;
+        }
+
+        // Update the "bullet"
+        //transform.position += transform.up * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Player/bulletMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Movement of "bullet" to detect collision */
+public class bulletMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    private float moveSpeed = 2.0F;
+
+    private float bulletRange = 10.0F;
+    private float distance;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Getting the distance between bullet and player
+        //if ((player.transform.position - transform.position).sqrMagnitude < 10.0F)
+        //{
+        //    Debug.Log("IS IN RANGE");
+        //    transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        //}
+        //else
+        //{
+        //    Debug.Log("IS OUT RANGE");
+        //}
+       
+    }
+}
