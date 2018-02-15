7241b15c93b98f5e805738af5fcdf80383f34421
Jolyn Wong <shiroyami08@gmail.com>
2018-02-15 09:20:18 +0800

    2D player simple movement

---
+++ Assets/Scripts/Player/2D/Player2DManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* For Player in 2D */
+public class Player2DManager : MonoBehaviour
+{
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement2D();
+
+		
+	}
+
+
+    /* Movement of Player */
+    void Movement2D()
+    {
+        // Up / Down
+        if (Input.GetKey(KeyCode.W))
+        {
+            //storePos.y +=  Time.deltaTime;
+            //transform.position += storePos;
+            transform.position += transform.up * 10.0F * Time.deltaTime;
+
+            Debug.Log(transform.position.ToString());
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.up * 10.0F * Time.deltaTime;
+        }
+
+        // Left / Right
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, 0, 90 * Time.deltaTime));
+            //transform.position -= transform.right * 10.0F * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 0, -90 * Time.deltaTime));
+            //transform.position += transform.right * 10.0F * Time.deltaTime;
+           
+
+        }
+
+
+    }
+}
