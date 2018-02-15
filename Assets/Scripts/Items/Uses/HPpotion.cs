db795f601381488c48a704c536051aa409e722bc
NICHOLAS POH JUN HENG <164353M@sidm.student.local>
2018-02-15 11:47:33 +0800

    Added Shop UI and Checks against Players when Purchasing. Require fix on UI positions and appearance

--- Assets/Scripts/Items/Uses/HPpotion.cs
+++ Assets/Scripts/Items/Uses/HPpotion.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HPpotion : MonoBehaviour, ItemBase
+public class HPpotion :  ItemBase
 {
     Sprite ItemImage;
     bool used;
