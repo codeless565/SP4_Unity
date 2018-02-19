af359d80c79303e4eca3824a59f3216d13a9fb5f
LIM PEI SHENG <164347E@sidm.student.local>
2018-02-19 13:08:31 +0800

    Completed DeadEndChecker(), not perfect, but will do for now

--- Assets/Scripts/Game/Level/LevelGenerator2D.cs
+++ Assets/Scripts/Game/Level/LevelGenerator2D.cs
@@ -31,9 +31,18 @@ public class LevelGenerator2D : MonoBehaviour
     float m_GridHalfSize;
 
     //Grid Occupancies
-    bool[,] m_HorizontalWall_occupied; //this is a 3D array
-    bool[,] m_VerticalWall_occupied; //this is a 3D array
-    bool[,] m_item_occupied; //this is a 3D array
+    private bool[,] m_HorizontalWall_occupied; //this is a 2D array
+    private bool[,] m_VerticalWall_occupied; //this is a 2D array
+    private bool[,] m_item_occupied; //this is a 2D array
+    private bool[,] m_visited;
+    private Stack<Vector2> m_stack;
+    private List<DeadEnd> m_deadEnds;
+
+    //Game Object Holder
+    private GameObject[,] m_horizontalWalls;
+    private GameObject[,] m_verticalWalls;
+
+    int m_deleteCount = 0;
 
     // Use this for initialization
     void Start()
@@ -43,26 +52,30 @@ public class LevelGenerator2D : MonoBehaviour
         //Size of grid 10x10
         m_GridSize = m_Wall.GetComponent<BoxCollider2D>().size.x;
         m_GridSingleOffSet = -(m_GridSize / (m_Wall.transform.childCount - 1) * 0.5f);
-        m_GridHalfSize = m_GridSingleOffSet * (m_Wall.transform.childCount * 0.5f);
-
-        Debug.Log("m_GridSingleOffSet: " + m_GridSingleOffSet);
+        m_GridHalfSize = m_GridSize * 0.5f + m_GridSingleOffSet;
 
         //Init Grid Occupancies Array
         int ArraySize = (int)(m_Floor.GetComponent<SpriteRenderer>().size.x / m_GridSize);
         m_HorizontalWall_occupied = new bool[ArraySize, ArraySize];
-        m_VerticalWall_occupied = new bool[ArraySize, ArraySize];
+        m_VerticalWall_occupied   = new bool[ArraySize, ArraySize];
         m_item_occupied = new bool[ArraySize, ArraySize];
+        m_visited = new bool[ArraySize, ArraySize];
+
+        m_horizontalWalls = new GameObject[ArraySize, ArraySize];
+        m_verticalWalls   = new GameObject[ArraySize, ArraySize];
+        m_deadEnds = new List<DeadEnd>();
 
         GenerateWalls();
+        DeadEndCheck(0, 0);
         //GenerateItem();
         Generatem_Exit();
         GeneratePlayerSpawn();
+        Debug.Log(m_deleteCount);
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     /*********************************
@@ -93,7 +106,7 @@ public class LevelGenerator2D : MonoBehaviour
             m_HorizontalWall_occupied[posX, posY] = true;
 
             Vector3 pos = new Vector3(posX * m_GridSize + m_GridSingleOffSet, posY * m_GridSize + m_GridSingleOffSet);
-            Instantiate(m_Wall, pos, transform.rotation, m_GameLevel.transform); //Instantiate(GameObject, Position, quaternion, Parent)
+            m_horizontalWalls[posX, posY] = Instantiate(m_Wall, pos, transform.rotation, m_GameLevel.transform); //Instantiate(GameObject, Position, quaternion, Parent)
         }
 
         //Vertical Walls
@@ -111,7 +124,7 @@ public class LevelGenerator2D : MonoBehaviour
             m_VerticalWall_occupied[posX, posY] = true;
 
             Vector3 pos = new Vector3(posX * m_GridSize + m_GridSingleOffSet, posY * m_GridSize + m_GridSingleOffSet);
-            Instantiate(m_Wall, pos, Quaternion.Euler(new Vector3(0, 0, 90)), m_GameLevel.transform); //Instantiate(GameObject, Position, quaternion, Parent)
+            m_verticalWalls[posX, posY] = Instantiate(m_Wall, pos, Quaternion.Euler(new Vector3(0, 0, 90)), m_GameLevel.transform); //Instantiate(GameObject, Position, quaternion, Parent)
         }
     }
 
@@ -130,7 +143,7 @@ public class LevelGenerator2D : MonoBehaviour
 
             m_HorizontalWall_occupied[posX, posY] = true;
             pos = new Vector3(posX * m_GridSize + m_GridSingleOffSet, posY * m_GridSize + m_GridSingleOffSet);
-            Instantiate(Wall, pos, transform.rotation, m_GameLevel.transform); //Instantiate(GameObject, Position, quaternion, Parent)
+            m_horizontalWalls[posX, posY] = Instantiate(Wall, pos, transform.rotation, m_GameLevel.transform); //Instantiate(GameObject, Position, quaternion, Parent)
 
             //out of range (back border)
             posX = x;
@@ -148,7 +161,7 @@ public class LevelGenerator2D : MonoBehaviour
 
             m_VerticalWall_occupied[posX, posY] = true;
             pos = new Vector3(posX * m_GridSize + m_GridSingleOffSet, posY * m_GridSize + m_GridSingleOffSet);
-            Instantiate(Wall, pos, Quaternion.Euler(new Vector3(0, 0, 90)), m_GameLevel.transform); //Instantiate(GameObject, Position, quaternion, Parent)
+            m_verticalWalls[posX, posY] = Instantiate(Wall, pos, Quaternion.Euler(new Vector3(0, 0, 90)), m_GameLevel.transform); //Instantiate(GameObject, Position, quaternion, Parent)
 
             //out of range (back border)
             posX = m_VerticalWall_occupied.GetLength(0);
@@ -174,8 +187,8 @@ public class LevelGenerator2D : MonoBehaviour
             do
             {
                 ++safetyCount;
-                posX = Random.Range(0, m_item_occupied.GetLength(0));
-                posY = Random.Range(0, m_item_occupied.GetLength(1));
+                posX = Random.Range(0, m_item_occupied.GetLength(0) - 1);
+                posY = Random.Range(0, m_item_occupied.GetLength(1) - 1);
             } while (m_item_occupied[posX, posY] && safetyCount < 50);
 
             if (m_item_occupied[posX, posY])
@@ -183,7 +196,7 @@ public class LevelGenerator2D : MonoBehaviour
 
             m_item_occupied[posX, posY] = true;
 
-            Vector3 temppos = new Vector3(posX * m_GridSize + m_GridSingleOffSet + m_GridHalfSize, posY * m_GridSize + m_GridSingleOffSet + m_GridHalfSize);
+            Vector3 temppos = new Vector3(posX * m_GridSize + m_GridHalfSize + m_GridSingleOffSet, posY * m_GridSize + m_GridHalfSize + m_GridSingleOffSet);
             Instantiate(Chest, temppos, Quaternion.Euler(new Vector3(0, 180, 0)), m_GameLevel.transform); //Instantiate(GameObject, Position, quaternion, Parent)
         }
     }
@@ -202,7 +215,7 @@ public class LevelGenerator2D : MonoBehaviour
         {
             ++safetyCount;
             posX = Random.Range(0, m_item_occupied.GetLength(0) - 1);
-            posY = Random.Range(1, m_item_occupied.GetLength(1));
+            posY = Random.Range(0, m_item_occupied.GetLength(1) - 1);
         } while (m_item_occupied[posX, posY] && safetyCount < 50);
 
         m_item_occupied[posX, posY] = true;
@@ -225,14 +238,12 @@ public class LevelGenerator2D : MonoBehaviour
         int posY;
         Vector3 pos;
 
-        float halfGridSize = m_GridSize * 0.5f;
-
         do
         {
             ++safetyCount;
             posX = Random.Range(0, m_item_occupied.GetLength(0) - 1);
-            posY = Random.Range(1, m_item_occupied.GetLength(1));
-            pos = new Vector3(posX * m_GridSize + halfGridSize, posY * m_GridSize + halfGridSize);
+            posY = Random.Range(0, m_item_occupied.GetLength(1) - 1);
+            pos = new Vector3(posX * m_GridSize + m_GridHalfSize + m_GridSingleOffSet, posY * m_GridSize + m_GridHalfSize + m_GridSingleOffSet);
         } while (m_item_occupied[posX, posY] || (pos - m_Exit.transform.position).sqrMagnitude <= MinDistanceFromExit);
 
         Debug.Log("SqrdMagnitude: " + (pos - m_Exit.transform.position).sqrMagnitude);
@@ -244,9 +255,123 @@ public class LevelGenerator2D : MonoBehaviour
     /*********************************
      * CHECKS *
      *********************************/
-    private void DeadEndCheck()
+    private void DeadEndCheck(int posX, int posY)
     {
+        //using DFS
+        //add curr to stack, mark as visited, call self to move next
+        //if is dead end and is not at the ends, save to list
+        //move back
 
+        //if no more possible move, check if all has been visited, if not, randomly delete 1 dead end and reset visit, call self agn from start
+
+        m_visited[posX, posY] = true;
+
+        //up
+        if (posY + 1 < m_HorizontalWall_occupied.GetLength(1) && !m_visited[posX, posY + 1])
+            if (m_HorizontalWall_occupied[posX, posY + 1])
+            {
+                m_deadEnds.Add(new DeadEnd(posX, posY + 1, 0));
+            }
+            else
+            {//Move
+
+                DeadEndCheck(posX, posY + 1);
+            }
+
+        //right
+        if (posX + 1 < m_VerticalWall_occupied.GetLength(0) && !m_visited[posX + 1, posY])
+            if (m_VerticalWall_occupied[posX + 1, posY])
+            {
+                m_deadEnds.Add(new DeadEnd(posX + 1, posY, 1));
+            }
+            else
+            {//Move
+                DeadEndCheck(posX + 1, posY);
+            }
+
+        //down
+        if (posY - 1 > 0)
+            if (m_HorizontalWall_occupied[posX, posY - 1] && !m_visited[posX, posY - 1])
+            {
+                m_deadEnds.Add(new DeadEnd(posX, posY - 1, 0));
+            }
+            else
+            {//Move 
+                DeadEndCheck(posX, posY - 1);
+            }
+
+        //left
+        if (posX - 1 > 0)
+            if (m_VerticalWall_occupied[posX - 1, posY] && !m_visited[posX - 1, posY])
+            {
+                m_deadEnds.Add(new DeadEnd(posX - 1, posY, 1));
+            }
+            else
+            {//Move 
+                DeadEndCheck(posX - 1, posY);
+            }
+
+        if (posX == 0 && posY == 0 && m_deleteCount < 100)
+        {
+            ++m_deleteCount;
+            bool notAllVisited = false;
+            for (int x = 0;x< m_visited.GetLength(0); ++x)
+                for (int y = 0; y < m_visited.GetLength(1); ++y)
+                {
+                    if (m_visited[x, y])
+                    {
+                        notAllVisited = true;
+                        break;
+                    }
+                }
+
+            //delete 1 random deadend wall and check
+            if(notAllVisited)
+            {
+                int chosenWall = Random.Range(0, m_deadEnds.Count);
+
+                //Delete the wall
+                if(m_deadEnds[chosenWall].type == 0)
+                {
+                    Destroy(m_horizontalWalls[m_deadEnds[chosenWall].GridX, m_deadEnds[chosenWall].GridY]);
+                }
+                else if (m_deadEnds[chosenWall].type == 1)
+                {
+                    Destroy(m_verticalWalls[m_deadEnds[chosenWall].GridX, m_deadEnds[chosenWall].GridY]);
+                }
+
+                ResetArray(m_visited, false);
+                m_deadEnds.Clear();
+                DeadEndCheck(0, 0);
+            }
+        }
+    }
+
+    private void ResetArray(bool[,] _array, bool value)
+    {
+        for (int x = 0; x < _array.GetLength(0); ++x)
+            for (int y = 0; y < _array.GetLength(1); ++y)
+            {
+                _array[x, y] = value;
+            }
+    }
+
+
+}
+
+struct DeadEnd
+{
+    public int GridX;
+    public int GridY;
+
+    // 0 - Horizontal, 1 - Vertical
+    public int type;
+
+    public DeadEnd(int _posX, int _posY, int _type)
+    {
+        GridX = _posX;
+        GridY = _posY;
+        type = _type;
     }
 }
 
