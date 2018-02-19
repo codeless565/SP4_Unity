6e9e632f05bb0b0b55163bdfedcc4bdded2ac7b0
LIM PEI SHENG <164347E@sidm.student.local>
2018-02-19 18:10:47 +0800

    Changed the sprite to fit the maze generation

---
+++ Assets/Scripts/Game/Level/PlayerSpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject Camera;
+    public bool RandomSpawn = false;
+    private GameObject floorholder; 
+    private Room[] rooms;
+
+	public void Init () {
+        floorholder = gameObject.GetComponent<BoardGenerator>().boardHolder;
+        rooms = gameObject.GetComponent<BoardGenerator>().GetRooms();
+
+        Vector3 playerpos;
+
+        if (RandomSpawn)
+        {
+            int randRm = Random.Range(0, rooms.Length - 1);
+            playerpos = new Vector3(rooms[randRm].xPos + rooms[randRm].roomWidth * 0.5f, rooms[randRm].yPos + rooms[randRm].roomHeight * 0.5f, 0);
+        }
+        else
+        {
+            playerpos = new Vector3(rooms[0].xPos + rooms[0].roomWidth * 0.5f, rooms[0].yPos + rooms[0].roomHeight * 0.5f, 0);
+        }
+
+        Camera.GetComponent<CameraController>().SetPlayer(Instantiate(player, playerpos, Quaternion.identity, floorholder.transform));
+    }
+}
