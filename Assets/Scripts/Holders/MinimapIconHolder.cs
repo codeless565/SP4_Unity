7c66f922f1dbd686bc6e4d927527bfe18aeef53f
LIM PEI SHENG <164347E@sidm.student.local>
2018-02-21 16:00:33 +0800

    Added Script for dicovering floor/item/exit/enemy to respective prefabs. Minimap will now show discovered item

---
+++ Assets/Scripts/Holders/MinimapIconHolder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapIconHolder : MonoBehaviour {
+
+    public Sprite Black;
+    public Sprite Floor;
+    public Sprite Chest;
+    public Sprite Exit;
+    public Sprite Enemy;
+}
