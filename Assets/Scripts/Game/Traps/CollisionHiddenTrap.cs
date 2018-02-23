3807dd2818145330bc7e9785703fd75f45204827
Jolyn Wong <shiroyami08@gmail.com>
2018-02-23 16:21:02 +0800

    stats menu edited attack nearer to player collision bear trap neater codes png for traps can be used

---
+++ Assets/Scripts/Game/Traps/CollisionHiddenTrap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Pops out a random trap when collide */
+public class CollisionHiddenTrap : MonoBehaviour, CollisionBase
+{
+    /* Will Implement the random traps soon */
+    /* For now, just render Bear trap when step over */
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /* Respomses to Collision */
+    public void CollisionResponse(string _tag)
+    {
+        throw new System.NotImplementedException();
+    }
+
+   
+}
