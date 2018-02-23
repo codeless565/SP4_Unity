06cfbabc6b0170fa776c508c4143273acc4926ce
LIM PEI SHENG <164347E@sidm.student.local>
2018-02-23 11:37:18 +0800

    Added Waypoint to enemyManager script

--- Assets/Scripts/Game/Level/ObjectSpawn.cs
+++ Assets/Scripts/Game/Level/ObjectSpawn.cs
@@ -4,36 +4,47 @@ using UnityEngine;
 
 public class ObjectSpawn : MonoBehaviour
 {
+    //Player
+    public  GameObject Player;
+    public  bool RandomPlayerSpawn = false;
+    private int m_playerRoom;
+    private Vector2 m_playerPos;
 
-    public GameObject Player;
-    public bool RandomPlayerSpawn = false;
-
+    //Main Camera/Player Camera
     public GameObject Camera;
 
+    //Chests
     public IntRange NumChest = new IntRange(5, 10);
+    public IntRange NumRoyalChest = new IntRange(0, 2);
+
+    //Enemy
+    public GameObject Enemy;
     public IntRange NumEnemy = new IntRange(5, 10);
 
+    //GO
     private Room[] m_rooms;
     private GameObject go_floorholder;
     private GameObject go_exit;
     private GameObject go_chest;
-    private GameObject go_chestMimic;
+    private GameObject go_royalchest;
 
-    private int m_playerRoom;
-    private Vector2 m_playerPos;
     private Vector2 m_exitPos;
+
+    private int m_currentFloor;
 
 
     public void Init(int _floor)
     {
-        go_floorholder = gameObject.GetComponent<BoardGenerator>().boardHolder;
-        m_rooms = gameObject.GetComponent<BoardGenerator>().GetRooms();
+        m_currentFloor = _floor;
 
-        if (_floor == 10)
-        {
-            //spawn Boss / bonus floor
-        }
-        else
+        go_floorholder = gameObject.GetComponent<BoardGenerator>().boardHolder;
+        m_rooms        = gameObject.GetComponent<BoardGenerator>().GetRooms();
+
+        //if (m_currentFloor == 10)
+        //{
+        //    //spawn Boss / bonus floor
+        //}
+        //else
         {
             PlayerSpawn();
             ExitSpawn();
@@ -51,7 +62,8 @@ public class ObjectSpawn : MonoBehaviour
 
         m_playerPos = new Vector2(m_rooms[m_playerRoom].xPos + m_rooms[m_playerRoom].roomWidth * 0.5f, m_rooms[m_playerRoom].yPos + m_rooms[m_playerRoom].roomHeight * 0.5f);
         GameObject t_player = Instantiate(Player, m_playerPos, Quaternion.identity, go_floorholder.transform); //Create Player Object
-        t_player.GetComponent<ObjectInfo>().Init(m_playerRoom, m_rooms[m_playerRoom], m_playerPos); //Set Starting Spawn location and detail to object
+        if (t_player.GetComponent<ObjectInfo>() != null)
+            t_player.GetComponent<ObjectInfo>().Init(m_playerRoom, m_rooms[m_playerRoom], m_playerPos); //Set Starting Spawn location and detail to object
 
         Camera.GetComponent<CameraController>().SetPlayer(t_player); //Spawn Player and Set the Instantiated player into Camera
     }
@@ -72,7 +84,8 @@ public class ObjectSpawn : MonoBehaviour
         m_exitPos = new Vector2(ranXpos, ranYpos);
 
         GameObject t_exit = Instantiate(go_exit, m_exitPos, Quaternion.identity, go_floorholder.transform);
-        t_exit.GetComponent<ObjectInfo>().Init(exitRoom, m_rooms[exitRoom], m_exitPos); //Set Starting Spawn location and detail to object
+        if (t_exit.GetComponent<ObjectInfo>() != null)
+            t_exit.GetComponent<ObjectInfo>().Init(exitRoom, m_rooms[exitRoom], m_exitPos); //Set Starting Spawn location and detail to object
 
         //Debug.Log("Exit Info: " + go_exit.GetComponent<ObjectInfo>().RoomIndex + "  " + go_exit.GetComponent<ObjectInfo>().MapPostion);
     }
@@ -103,14 +116,66 @@ public class ObjectSpawn : MonoBehaviour
             } while (tempPos == m_playerPos || tempPos == m_exitPos);
 
             GameObject tempChest = Instantiate(go_chest, tempPos, Quaternion.identity, go_floorholder.transform);
-            tempChest.GetComponent<ObjectInfo>().Init(tempRoom, m_rooms[tempRoom], tempPos); //Set Starting Spawn location and detail to object
-
-            //Debug.Log("Chest Info: " + tempChest.GetComponent<ObjectInfo>().RoomIndex + "  " + tempChest.GetComponent<ObjectInfo>().MapPostion);
+            if (tempChest.GetComponent<ObjectInfo>() != null)
+                tempChest.GetComponent<ObjectInfo>().Init(tempRoom, m_rooms[tempRoom], tempPos); //Set Starting Spawn location and detail to object
         }
     }
 
     private void EnemySpawn(int _floor)
     {
         //spawn enemy and init their level based on the curr floor's level
+        /*
+         * Spawn enemy in the room, give 4 vector3 in a array as waypoint , waypoint are corners of the room
+         * */
+
+        int AmtOfEnemy = NumEnemy.Random;
+
+        // Safety Check
+        if (AmtOfEnemy >= m_rooms.Length)
+        {
+            AmtOfEnemy = m_rooms.Length - 2;
+        }
+
+        // Temp values as holder
+        int tempRoom;
+        Vector2 tempPos = new Vector2(0, 0);
+
+        // Spawn the Number of Enemies in loop
+        for (int i = 0; i < AmtOfEnemy; ++i)
+        {
+            do
+            {
+                // Choose a random room
+                tempRoom = Random.Range(0, m_rooms.Length - 1);
+            } while (tempRoom == m_playerRoom);
+
+            // Setup waypoints
+            Vector3[] Waypoint = new Vector3[4];
+            Waypoint[0] = new Vector3(m_rooms[tempRoom].xPos, m_rooms[tempRoom].yPos);
+            Waypoint[1] = new Vector3(m_rooms[tempRoom].xPos, m_rooms[tempRoom].yPos + m_rooms[tempRoom].roomHeight - 1);
+            Waypoint[2] = new Vector3(m_rooms[tempRoom].xPos + m_rooms[tempRoom].roomWidth - 1, m_rooms[tempRoom].yPos + m_rooms[tempRoom].roomHeight - 1);
+            Waypoint[3] = new Vector3(m_rooms[tempRoom].xPos + m_rooms[tempRoom].roomWidth - 1, m_rooms[tempRoom].yPos);
+            Debug.Log("Enemy " + i + ": " + Waypoint[0]);
+            Debug.Log("Enemy " + i + ": " + Waypoint[1]);
+            Debug.Log("Enemy " + i + ": " + Waypoint[2]);
+            Debug.Log("Enemy " + i + ": " + Waypoint[3]);
+
+            int randomID = Random.Range(0, Waypoint.Length - 1);
+            tempPos = Waypoint[randomID];
+
+            // Instantiate Object, Set waypoint and Set room info into ObjectInfo
+            GameObject tempEnemy = Instantiate(Enemy, tempPos, Quaternion.identity, go_floorholder.transform);
+            if (tempEnemy.GetComponent<SkeletonEnemyManager>() != null)
+            {
+                //Set EXP Reward for enemy 
+                tempEnemy.GetComponent<SkeletonEnemyManager>().EXPReward = tempEnemy.GetComponent<SkeletonEnemyManager>().EXPRewardScaling * m_currentFloor;
+                tempEnemy.GetComponent<SkeletonEnemyManager>().Waypoint = Waypoint;
+                tempEnemy.GetComponent<SkeletonEnemyManager>().CurrWaypointID = randomID;
+            }
+
+            if (tempEnemy.GetComponent<ObjectInfo>() != null)
+                tempEnemy.GetComponent<ObjectInfo>().Init(tempRoom, m_rooms[tempRoom], tempPos); //Set Starting Spawn location and detail to object
+        }
+
     }
 }
