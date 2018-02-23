c8e8ae3ff422e4045476663cc6d11b93f4dd92f3
LIM PEI SHENG <164347E@sidm.student.local>
2018-02-23 19:03:14 +0800

    Added SlowTrap Made changes to allow editing of value of trap after instantiating it. DamageTrap will use another function duing ObjectSpawn to scale damage with floor

---
+++ Assets/Scripts/Game/Traps/SlowDownTrapEffect.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDownTrapEffect : MonoBehaviour {
+
+    private float m_multiplier = 0.2f;
+    private float m_duration = 5;
+    private float m_elapseTime;
+    private float m_originalSpeed;
+    private float m_slowedSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_elapseTime = m_duration;
+        m_originalSpeed = GetComponent<StatsBase>().MoveSpeed;
+        m_slowedSpeed = m_originalSpeed * m_multiplier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_elapseTime -= Time.deltaTime;
+
+        if (m_elapseTime <= 0)
+        {
+            GetComponent<StatsBase>().MoveSpeed = m_originalSpeed;
+            Destroy(this);
+            return;
+        }
+
+        GetComponent<StatsBase>().MoveSpeed = m_slowedSpeed;
+    }
+
+    public void SetDuration(float _value)
+    {
+        m_duration = _value;
+        m_elapseTime = m_duration;
+    }
+
+    public void ResetTimer()
+    {
+        m_elapseTime = m_duration;
+    }
+}
