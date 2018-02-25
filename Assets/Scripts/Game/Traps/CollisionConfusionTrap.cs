eabfe9c5a8a2e051231a8db814389044c718be5c
Jolyn Wong <shiroyami08@gmail.com>
2018-02-26 00:39:20 +0800

    Did a simple confusion Trap ( players controls becomes opp when touched ) will be trying to random the moves of the player

--- Assets/Scripts/Game/Traps/CollisionConfusionTrap.cs
+++ Assets/Scripts/Game/Traps/CollisionConfusionTrap.cs
@@ -3,26 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /* Confuse Player and messes their controls */
-public class CollisionConfusionTrap : MonoBehaviour, CollisionBase
+/* Specific to player */
+public class CollisionConfusionTrap : MonoBehaviour
 {
-    /* Get Player Movements from Manager */
+    /* Modifiers to adjust player movement */
+    static public int m_confusedModifier;
+
+    /* Timer for Confused Effect */
+    private float m_fDuration;
+    private bool m_isActivated;
 
     // Use this for initialization
     void Start()
     {
+        m_confusedModifier = 1;
 
+        m_fDuration = 5.0F;
+        m_isActivated = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (m_isActivated)
+        {
+            m_fDuration -= Time.deltaTime;
 
+            /* For Rendering the Sprite */
+            if (m_fDuration <= 0.0F)
+            {
+                Destroy(gameObject);
+                m_confusedModifier = 1;
+                m_fDuration = 5.0f;
+            }
+        }
     }
 
-    public void CollisionResponse(string _tag)
+    /* When Collide, Confuse the movement */
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        throw new System.NotImplementedException();
-    }
+        /* If Only want player to be affected */
+        if (collision.GetComponent<Player2D_Manager>() == null)
+            return;
 
-    
+        /* Only Once */
+        if (m_isActivated)
+            return;
+
+        /* Set Timer for Duration of Confusion */
+        m_confusedModifier = -1;
+        m_isActivated = true;
+    }
 }
