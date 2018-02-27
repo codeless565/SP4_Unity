2408f834a7146e59974ef550dbb3c646af052b12
Jolyn Wong <shiroyami08@gmail.com>
2018-02-27 16:12:28 +0800

    Edited Merchant Things Nid to random Spawn and Test Merchant

---
+++ Assets/Scripts/Merchant/Merchant_GoodBye.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* When Merchant is in GoodBye, Render GoodBye Text
+   When Press Text Box, Changed to IDLE */
+public class Merchant_GoodBye : MonoBehaviour
+{
+    /* Dialouge With Merchant */
+    private TextBoxManager _theManager;
+    public TextAsset theText;
+    public int startLine;
+    public int endLine;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/Merchant/Merchant_Idle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* When Merchant is in Idle, Check for TriggerStay + Button in State Machine
+   When Checked, Changed to INTERACT */
+public class Merchant_Idle : MonoBehaviour
+{
+    //private void Start()
+    //{
+    //    if (GetComponentInParent<MerchantStateMachine>().m_state != "IDLE")
+    //        return;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+
+    //    //Debug.Log(GetComponentInParent<MerchantStateMachine>().m_state);
+    //}
+}
