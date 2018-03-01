9eb56763fc9ae1e5f03727ec22e3e1f5df2dd13e
Pei Sheng <peisheng1@hotmail.com>
2018-03-02 02:30:31 +0800

    Added Animation to UI Bars

--- Assets/Scripts/Game/HUD/PlayerExpBar.cs
+++ Assets/Scripts/Game/HUD/PlayerExpBar.cs
@@ -9,15 +9,48 @@ public class PlayerExpBar : MonoBehaviour
 
     GameObject m_player;
 
+    float m_currvalue;
+    float m_movingvalue;
+    float m_newvalue;
+
     public void Init(GameObject _player)
     {
         m_player = _player;
+        m_movingvalue = m_player.GetComponent<StatsBase>().EXP;
+        m_newvalue = m_movingvalue;
     }
 
     void Update()
     {
+        m_currvalue = m_player.GetComponent<StatsBase>().EXP;
+
+        //Deduct the hp overtime for animation
+        if (m_movingvalue != m_newvalue)
+        {
+            if (m_movingvalue >= m_newvalue)
+            {
+                m_movingvalue -= m_player.GetComponent<StatsBase>().MaxEXP * Time.deltaTime;
+                if (m_movingvalue <= m_newvalue)
+                    m_movingvalue = m_newvalue;
+            }
+            else if (m_movingvalue <= m_newvalue)
+            {
+                m_movingvalue += m_player.GetComponent<StatsBase>().MaxEXP * Time.deltaTime;
+                if (m_movingvalue >= m_newvalue)
+                    m_movingvalue = m_newvalue;
+            }
+        }
+
         // Update HP Bar
-        GetComponent<Slider>().value = m_player.GetComponent<StatsBase>().EXP / m_player.GetComponent<StatsBase>().MaxEXP;
+        GetComponent<Slider>().value = m_movingvalue / m_player.GetComponent<StatsBase>().MaxEXP;
+    }
+
+    void LateUpdate()
+    {
+        if (m_player.GetComponent<StatsBase>().EXP <= m_currvalue)
+        {
+            m_newvalue = m_player.GetComponent<StatsBase>().EXP;
+        }
     }
 }
 
--- Assets/Scripts/Game/HUD/PlayerStaminaBar.cs
+++ Assets/Scripts/Game/HUD/PlayerStaminaBar.cs
@@ -9,14 +9,47 @@ public class PlayerStaminaBar : MonoBehaviour
 
     GameObject m_player;
 
+    float m_currvalue;
+    float m_movingvalue;
+    float m_newvalue;
+
     public void Init(GameObject _player)
     {
         m_player = _player;
+        m_movingvalue = m_player.GetComponent<StatsBase>().Stamina;
+        m_newvalue = m_movingvalue;
     }
 
     void Update()
     {
+        m_currvalue = m_player.GetComponent<StatsBase>().Stamina;
+
+        //Deduct the hp overtime for animation
+        if (m_movingvalue != m_newvalue)
+        {
+            if (m_movingvalue >= m_newvalue)
+            {
+                m_movingvalue -= m_player.GetComponent<StatsBase>().MaxStamina * Time.deltaTime;
+                if (m_movingvalue <= m_newvalue)
+                    m_movingvalue = m_newvalue;
+            }
+            else if (m_movingvalue <= m_newvalue)
+            {
+                m_movingvalue += m_player.GetComponent<StatsBase>().MaxStamina * Time.deltaTime;
+                if (m_movingvalue >= m_newvalue)
+                    m_movingvalue = m_newvalue;
+            }
+        }
+
         // Update HP Bar
-        GetComponent<Slider>().value = m_player.GetComponent<StatsBase>().Stamina / m_player.GetComponent<StatsBase>().MaxStamina;
+        GetComponent<Slider>().value = m_movingvalue / m_player.GetComponent<StatsBase>().MaxStamina;
+    }
+
+    void LateUpdate()
+    {
+        if (m_player.GetComponent<StatsBase>().Stamina <= m_currvalue)
+        {
+            m_newvalue = m_player.GetComponent<StatsBase>().Stamina;
+        }
     }
 }
