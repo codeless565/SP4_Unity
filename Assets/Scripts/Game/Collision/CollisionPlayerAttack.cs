cf3585df557b1c2ba07d3d69828f6efca412998f
LIM PEI SHENG <164347E@sidm.student.local>
2018-03-01 20:01:12 +0800

    Made Special Mechanics for boss, Player cannot directly attack boss but by reflecting ball fired by boss, Added Sword Clash Particles, Added Projectile Hit Effect, Added LevelUpEffect Particles for use, Reduced collision radius for player and ConfusionTrap,

--- Assets/Scripts/Game/Collision/CollisionPlayerAttack.cs
+++ Assets/Scripts/Game/Collision/CollisionPlayerAttack.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class CollisionPlayerAttack : MonoBehaviour {
 
+    GameObject m_owner;
     float m_damage;
 
-    public void Init(float _damage)
+    public void Init(float _damage, GameObject _owner)
     {
+        m_owner = _owner;
         m_damage = _damage;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.GetComponent<BossStatsManager>() != null ||
-           other.GetComponent<SkeletonEnemyManager>() != null)
+        if (other.GetComponent<SkeletonEnemyManager>() != null || other.GetComponent<ProjectileObject>() != null)
         {
-            // Deals damage to the enemy
-            other.GetComponent<StatsBase>().Health -= Calculator.Instance.CalculateDamage(m_damage, other.GetComponent<StatsBase>().Defense);
+            //Get the Particle effect
+            GameObject t_effect = GameObject.FindGameObjectWithTag("Holder").GetComponent<StructureObjectHolder>().SwordClashEffect;
+
+            // Calculate the rotation from player to enemy
+            float effectArc = t_effect.GetComponent<ParticleSystem>().shape.arc * 0.5f;
+            Vector3 rotationVec = other.transform.position - m_owner.transform.position;
+            float Angle = Mathf.Atan2(rotationVec.y, rotationVec.x) * Mathf.Rad2Deg;
+
+            // Create sword clash effect
+            GameObject tempEffect = Instantiate(t_effect, other.transform.position, Quaternion.Euler(0, 0, Angle - effectArc)) as GameObject;
+
+            if (other.GetComponent<StatsBase>() != null)
+            {// Deals damage to the enemy
+                other.GetComponent<StatsBase>().Health -= Calculator.Instance.CalculateDamage(m_damage, other.GetComponent<StatsBase>().Defense);
+            }
         }
     }
 }
--- Assets/Scripts/Game/Projectile/ProjectileObject.cs
+++ Assets/Scripts/Game/Projectile/ProjectileObject.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ProjectileObject : MonoBehaviour
+public class ProjectileObject : MonoBehaviour, Projectile
 {
     GameObject m_owner;
 
     Vector3 m_dir;
     float m_damage;
     float m_speed;
+    bool m_reflected;
 
 	// Use this for initialization
 	public void Init (GameObject _owner, Vector3 _dir, float _damage, float _speed = 10)
@@ -18,7 +20,8 @@ public class ProjectileObject : MonoBehaviour
 //        Debug.Log(m_dir);
         m_damage = _damage;
         m_speed = _speed;
-	}
+        m_reflected = false;
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -33,10 +36,23 @@ public class ProjectileObject : MonoBehaviour
 
         if (other.GetComponent<StatsBase>() != null)
         {
-            other.GetComponent<StatsBase>().Health -= m_damage;
-        }
+            GameObject t_effect = GameObject.FindGameObjectWithTag("Holder").GetComponent<StructureObjectHolder>().ShadowBlastEffect;
 
-        Destroy(gameObject); // if it hits wall also destroy
-        //can instatiate particles here
+            // Create blast effect
+            GameObject tempEffect = Instantiate(t_effect, other.transform.position, Quaternion.identity) as GameObject;
+
+            other.GetComponent<StatsBase>().Health -= m_damage;
+            Destroy(gameObject); // if it hits wall also destroy
+       }
+
+        if (other.GetComponent<CollisionPlayerAttack>() != null && !m_reflected)
+        {
+            m_reflected = true;
+            m_owner = other.transform.parent.gameObject;
+            m_dir = other.transform.up;     //Reverse the direction of the ball
+            Debug.Log("Changed Dir: " + m_dir);
+            m_damage = other.GetComponentInParent<StatsBase>().Attack;
+            GetComponent<SpriteRenderer>().sprite = GameObject.FindGameObjectWithTag("Holder").GetComponent<StructureObjectHolder>().PlayerCounterBall;
+        }
     }
 }
