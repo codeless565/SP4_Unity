4489f4a82e810266473c3f20a013af77c5e2662f
Pei Sheng <peisheng1@hotmail.com>
2018-03-02 02:49:39 +0800

    Added Particle Destroy script to Destroy Particle effect once duration is up. Added Script to Particle effects to remove from game once duration is up. LevelupEffect has not been changed

---
+++ Assets/Scripts/Game/ParticleDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestroy : MonoBehaviour 
+{
+    float m_Duration;
+	// Use this for initialization
+	void Start ()
+    {
+        m_Duration = GetComponent<ParticleSystem>().main.duration;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_Duration -= Time.deltaTime;
+        if (m_Duration <= 0)
+            Destroy(gameObject);
+	}
+}
