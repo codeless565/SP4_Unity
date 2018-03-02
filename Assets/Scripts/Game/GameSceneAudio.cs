2fcee9ca23dee5505386aa2e9bd61a30a7449ff5
GOH LIANGLI <163464J@sidm.student.local>
2018-03-02 17:08:43 +0800

    Added in Audio Script.

---
+++ Assets/Scripts/Game/GameSceneAudio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneAudio : MonoBehaviour
+{
+    [SerializeField]
+    GameObject backgroundMusic;
+
+    // Background Music
+    private AudioSource musicSource;
+    
+	void Start ()
+    {
+        // Background Music
+        musicSource = backgroundMusic.GetComponent<AudioSource>();
+        // Get Background Music
+        musicSource.volume = PlayerPrefs.GetFloat("BGM_Volume", 1);
+	}
+	
+	void Update ()
+    {
+        // Get Background Music
+        musicSource.volume = PlayerPrefs.GetFloat("BGM_Volume", 1);
+    }
+}
